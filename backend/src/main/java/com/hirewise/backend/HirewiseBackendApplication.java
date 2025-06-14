dcf46eec2cc0821774f6fc86d63a31e23b5bbf8c
Mr.Tanmay <tanmaykadam8828@gmail.com>
2025-06-14 17:28:37 +0530

    project setup

---
+++ backend/src/main/java/com/hirewise/backend/HirewiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hirewise.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirewiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirewiseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hirewise/backend/HirewiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirewise.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirewiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
